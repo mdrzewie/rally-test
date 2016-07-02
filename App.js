8615fcacdf8cda3c71e2b331787bbb09bdde2fd0
Mike D <mdrzewie@gmail.com>
2016-07-02 22:11:07 +0000

    First commit

---
+++ App.js
@@ -0,0 +1,16 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items : [ { 
+        xtype: 'rallybutton', 
+        text: 'Click me', 
+        handler: function() { 
+            Ext.Msg.alert('Button', 'You clicked me'); 
+        } 
+    } ],
+    launch: function() {
+        //Write app code here
+
+        //API Docs: https://help.rallydev.com/apps/2.1/doc/
+    }
+});
